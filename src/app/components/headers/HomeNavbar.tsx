d0fd058814c2f2582b6fc0ea908da45679bb1f00
Edward_MIT <usmanoff19980404@gmail.com>
2025-02-07 16:10:58 +0900

    feat: develop AppRootState and HomePage type integrations

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 export default function HomeNavbar () {
  const authMember = null;
  const [count, setCount] = useState<number>(0);
- const [value, setvalue] = useState<boolean>(true);
+ const [value, setvalue] = useState<any>({status: true});
 
 
   useEffect(() => {
