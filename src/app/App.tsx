d7d36864402297a7bc6d549d766ea3a08bf7b9af
Edward_MIT <usmanoff19980404@gmail.com>
2025-02-18 21:22:27 +0900

    feat:publish authentication modal companent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,18 +8,26 @@ import  UserPage  from "./screens/usersPage";
 import  HomeNavbar from "./components/headers/HomeNavbar";
 import  OtherNavbar  from "./components/headers/OtherNavbar";
 import  Footer  from "./components/footer";
+import  HelpPage  from "./screens/helpPage";
+import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
 
-import  HelpPage  from "./screens/helpPage";
-import useBasket from "./hooks/useBasket";
-
 
 function App() {
  const location = useLocation();
-
  const {cartItems, onAdd, onRemove, onDelete, onDeleteAll} = useBasket();
+ const  [signupOpen, setSignupOpen] = useState<boolean>(false);
+ const  [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+ /** HANDLERS **/
+
+ const handleSignupClose = () => setSignupOpen(false);
+ const handleLoginClose = () => setLoginOpen(false);
+
+
 
   return (
     <>
@@ -58,6 +66,14 @@ function App() {
        </Route>
      </Switch>
      <Footer/>
+
+     <AuthenticationModal
+      signupOpen={signupOpen}
+      loginOpen= {loginOpen}
+      handleLoginClose={handleLoginClose}
+      handleSignupClose={handleSignupClose}
+     />
+
     </>
   );
 }
